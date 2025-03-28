6d8a177bb3cbeb7d46989da1b7f2b04f8349b638
Minhfansub <phambinhminhhk@gmail.com>
2025-03-28 12:00:28 +0700

    Update test hac scriptminhfansub.lua

--- scriptminhfansub.lua
+++ scriptminhfansub.lua
@@ -321,9 +321,6 @@ local v15 = v14:CreateWindow({
     Size = UDim2.fromOffset(500, 320),
     MinimizeKey = Enum.KeyCode.End
 });
-local v16 = {
-    Home = v15:AddTab({
-        Title = "Thông Tin"
     }),
     Main = v15:AddTab({
         Title = "Cày"
